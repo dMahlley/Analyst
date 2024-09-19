c2224bbbd69ffc624f6ba492aa57aed8e6a32a00
Devyani Mahalle <59561727+dMahlley@users.noreply.github.com>
2024-09-19 18:55:31 +0530

    Add files via upload
    The project indicates the analysis of energy consumption and shows it with the dashboard.

---
+++ energy_consummation.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE energyDB;
+
+USE energyDB;
+
+SHOW TABLES;
+SELECT * FROM `energy-consummation-data` ;
---
+++ energy_consummation_data.sql
@@ -0,0 +1,7 @@
+ CREATE DATABASE energyDB;
+ 
+ USE energyDB;
+ SHOW TABLES;
+ 
+ SELECT * FROM `energy-consummation-data`;
+ 
